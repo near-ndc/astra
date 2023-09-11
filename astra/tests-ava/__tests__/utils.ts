c52c1ebd0e0a46985d6750e4b8cf5333c605ad40
Amit Yadav <amy29981@gmail.com>
2023-09-11 17:15:48 +0530

    path

--- astra/tests-ava/__tests__/utils.ts
+++ astra/tests-ava/__tests__/utils.ts
@@ -38,7 +38,7 @@ export const workspaceWithoutInit = Workspace.init(async ({ root }) => {
 export const workspaceWithFactory = Workspace.init(async ({ root }) => {
     const factory = await root.createAndDeploy(
         'factory',
-        '../../../astra-factory/res/astra_factory.wasm',
+        '../../res/astra_factory.wasm',
         {
             initialBalance: toYocto('500'),
         },
@@ -50,7 +50,7 @@ export const workspaceWithFactory = Workspace.init(async ({ root }) => {
 export async function initTestToken(root: NearAccount) {
     const testToken = await root.createAndDeploy(
         'test-token',
-        '../../../test-token/res/test_token.wasm',
+        '../../res/test_token.wasm',
         {
             method: 'new',
             initialBalance: toYocto('200'),
@@ -66,7 +66,7 @@ export async function initStaking(
 ) {
     const staking = await root.createAndDeploy(
         'staking',
-        '../../../astra-staking/res/astra_staking.wasm',
+        '../../res/astra_staking.wasm',
         {
             method: 'new',
             args: {
